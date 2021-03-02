bf2e27ff9b603cc7bde71edb5236ce1c6c514796
Quentin Vincent <nozzy@admin.club-rezo.net>
2021-03-02 14:44:18 +0100

    update 02032021.1

--- bot.py
+++ bot.py
@@ -20,7 +20,6 @@ intents = discord.Intents.default()
 intents.members = True
 client = discord.Client()
 bot = commands.Bot(command_prefix="--", description="Le p'tit bot !")
-TOKEN = "NjUzNTYzMTQxMDAyNzU2MTA2.Xe40Gw.wwbsdL-JhaxgeeMiUn89t0dIvTk"
 nbtg: int = 0
 
 
